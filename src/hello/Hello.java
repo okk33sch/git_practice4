05433231fddc3c0930bc1fdd4a6be3958c0f9ab5
okk33sch <korayozbex@gmail.com>
2022-01-24 10:39:38 -0500

    doing ginal work on hello feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,4 +1,10 @@
 package hello;
 
 public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.print("B25 LEARNING GIT AND GITHUB");
+    }
 }
